5d0c32511092bfb9c364b1a6dc732ab7530a10f9
akinloluwami <akinkunmioye42@gmail.com>
2024-11-02 11:39:58 +0100

    feat: create app header component

---
+++ app/components/app-header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AppHeader = ({ title }: { title: string }) => {
+  return (
+    <div className="bg-white shadow-sm w-full px-4 sm:px-6 lg:px-8 py-4 sticky top-0 z-10">
+      <h2 className="text-xl font-semibold">{title}</h2>
+    </div>
+  );
+};
+
+export default AppHeader;
