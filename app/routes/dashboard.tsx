9e105fb43bd0dcefde56db96f5de65287a6e9c65
akinloluwami <akinkunmioye42@gmail.com>
2024-11-02 11:41:04 +0100

    feat: create dashboard layout component

---
+++ app/routes/dashboard.tsx
@@ -0,0 +1,18 @@
+import { Outlet } from "@remix-run/react";
+import { AppSidebar } from "~/components/app-sidebar";
+import { SidebarProvider, SidebarTrigger } from "~/components/ui/sidebar";
+
+const Dashboard = () => {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main className="w-full">
+        <div className="">
+          <Outlet />
+        </div>
+      </main>
+    </SidebarProvider>
+  );
+};
+
+export default Dashboard;
