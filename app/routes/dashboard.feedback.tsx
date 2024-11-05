c5908301400110d066812d6a3d99c1edb9564591
akinloluwami <akinkunmioye42@gmail.com>
2024-11-05 02:42:09 +0100

    feat: did a whole lot of stuff

--- app/routes/dashboard.feedback.tsx
+++ app/routes/dashboard.feedback.tsx
@@ -1,5 +1,8 @@
 import AppHeader from "~/components/app-header";
 import { useState } from "react";
+import { json, LoaderFunction } from "@remix-run/node";
+import { useLoaderData, Link, useNavigate } from "@remix-run/react";
+import { prisma } from "prisma/client";
 import { MoreHorizontal } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import { Button } from "~/components/ui/button";
@@ -34,73 +37,79 @@ import {
   DropdownMenuTrigger,
 } from "~/components/ui/dropdown-menu";
 import { MetaFunction } from "@remix-run/react";
+import { getCookie } from "~/lib/cookies";
+import { getUserIdFromToken } from "~/lib/auth";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "~/components/ui/dialog";
+import { Label } from "~/components/ui/label";
 
-const feedbackData = [
-  {
-    id: 1,
-    user: "Alice Johnson",
-    email: "alice@example.com",
-    sentiment: "Positive",
-    feedback:
-      "The new feature is amazing! It has greatly improved my workflow.",
-    date: "2023-06-15",
-    status: "New",
-  },
-  {
-    id: 2,
-    user: "Bob Smith",
-    email: "bob@example.com",
-    sentiment: "Neutral",
-    feedback:
-      "The app works fine, but I think there's room for improvement in the user interface.",
-    date: "2023-06-14",
-    status: "In Progress",
-  },
-  {
-    id: 3,
-    user: "Charlie Brown",
-    email: "charlie@example.com",
-    sentiment: "Negative",
-    feedback:
-      "I encountered a bug that prevented me from saving my work. This is a critical issue that needs to be addressed.",
-    date: "2023-06-13",
-    status: "Resolved",
-  },
-  {
-    id: 4,
-    user: "Diana Prince",
-    email: "diana@example.com",
-    sentiment: "Positive",
-    feedback:
-      "The customer support team was incredibly helpful in resolving my issue. Great service!",
-    date: "2023-06-12",
-    status: "New",
-  },
-  {
-    id: 5,
-    user: "Ethan Hunt",
-    email: "ethan@example.com",
-    sentiment: "Neutral",
-    feedback:
-      "The new update is okay, but I preferred some aspects of the previous version.",
-    date: "2023-06-11",
-    status: "In Progress",
-  },
-];
+export const loader: LoaderFunction = async ({ request }) => {
+  const url = new URL(request.url);
+  const page = Number(url.searchParams.get("page") || 1);
+  const sentiment = url.searchParams.get("sentiment") || ("all" as any);
+  const itemsPerPage = 10;
+
+  const whereCondition = sentiment === "all" ? {} : { sentiment };
+  const projectId = getCookie(request, "unecho.project-id")!;
+  const userToken = getCookie(request, "unecho.auth-token")!;
+  const userId = getUserIdFromToken(userToken)!;
+
+  const feedbackData = await prisma.feedback.findMany({
+    where: {
+      ...whereCondition,
+      projectId,
+      project: {
+        userId,
+      },
+    },
+    skip: (page - 1) * itemsPerPage,
+    take: itemsPerPage,
+    include: {
+      clientUserProfile: {
+        select: { name: true, email: true, avatar: true },
+      },
+    },
+    orderBy: { createdAt: "desc" },
+  });
+
+  const totalFeedbacks = await prisma.feedback.count({ where: whereCondition });
+  const totalPages = Math.ceil(totalFeedbacks / itemsPerPage);
+
+  return json({
+    feedbackData: feedbackData.map((feedback) => ({
+      id: feedback.id,
+      user: feedback?.clientUserProfile?.name || "",
+      email: feedback.clientUserProfile?.email || "",
+      avatar: feedback.clientUserProfile?.avatar || "",
+      sentiment: feedback.sentiment,
+      score: feedback.score,
+      feedback: feedback.content,
+      date: feedback.createdAt.toISOString().split("T")[0],
+    })),
+    page,
+    hasNextPage: page < totalPages,
+    hasPreviousPage: page > 1,
+    sentiment,
+  });
+};
 
 const Feedback = () => {
-  const [selectedStatus, setSelectedStatus] = useState("All");
-  const [selectedSentiment, setSelectedSentiment] = useState("All");
-  const [searchTerm, setSearchTerm] = useState("");
+  const { feedbackData, page, hasNextPage, hasPreviousPage, sentiment } =
+    useLoaderData<typeof loader>();
+  const [selectedSentiment, setSelectedSentiment] = useState(sentiment);
+  const navigate = useNavigate();
 
-  const filteredFeedback = feedbackData.filter(
-    (feedback) =>
-      (selectedStatus === "All" || feedback.status === selectedStatus) &&
-      (selectedSentiment === "All" ||
-        feedback.sentiment === selectedSentiment) &&
-      (feedback.feedback.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        feedback.user.toLowerCase().includes(searchTerm.toLowerCase()))
-  );
+  const handleSentimentChange = (value: string) => {
+    setSelectedSentiment(value);
+    navigate(`?page=1&sentiment=${value}`);
+  };
+
   return (
     <div>
       <AppHeader title="Feedback" />
@@ -114,31 +123,17 @@ const Feedback = () => {
               </div>
               <div className="flex space-x-2">
                 <Select
-                  value={selectedStatus}
-                  onValueChange={setSelectedStatus}
-                >
-                  <SelectTrigger className="w-[180px]">
-                    <SelectValue placeholder="Filter by status" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="All">All Statuses</SelectItem>
-                    <SelectItem value="New">New</SelectItem>
-                    <SelectItem value="In Progress">In Progress</SelectItem>
-                    <SelectItem value="Resolved">Resolved</SelectItem>
-                  </SelectContent>
-                </Select>
-                <Select
                   value={selectedSentiment}
-                  onValueChange={setSelectedSentiment}
+                  onValueChange={handleSentimentChange}
                 >
                   <SelectTrigger className="w-[180px]">
                     <SelectValue placeholder="Filter by sentiment" />
                   </SelectTrigger>
                   <SelectContent>
-                    <SelectItem value="All">All Sentiments</SelectItem>
-                    <SelectItem value="Positive">Positive</SelectItem>
-                    <SelectItem value="Neutral">Neutral</SelectItem>
-                    <SelectItem value="Negative">Negative</SelectItem>
+                    <SelectItem value="all">All Sentiments</SelectItem>
+                    <SelectItem value="positive">Positive</SelectItem>
+                    <SelectItem value="neutral">Neutral</SelectItem>
+                    <SelectItem value="negative">Negative</SelectItem>
                   </SelectContent>
                 </Select>
               </div>
@@ -150,19 +145,26 @@ const Feedback = () => {
                 <TableRow>
                   <TableHead>User</TableHead>
                   <TableHead>Sentiment</TableHead>
+                  <TableHead>Score</TableHead>
                   <TableHead>Feedback</TableHead>
                   <TableHead>Date</TableHead>
                   <TableHead>Actions</TableHead>
                 </TableRow>
               </TableHeader>
               <TableBody>
-                {filteredFeedback.map((feedback) => (
+                {feedbackData.map((feedback: any) => (
                   <TableRow key={feedback.id}>
                     <TableCell>
                       <div className="flex items-center space-x-2">
                         <Avatar className="h-8 w-8">
+                          <AvatarImage
+                            src={feedback.avatar}
+                            className="w-full h-full object-cover"
+                          />
                           <AvatarFallback>
-                            {feedback.user.charAt(0)}
+                            {feedback.user.charAt(0) ||
+                              feedback.email.charAt(0) ||
+                              "-"}
                           </AvatarFallback>
                         </Avatar>
                         <div>
@@ -176,9 +178,9 @@ const Feedback = () => {
                     <TableCell>
                       <span
                         className={`inline-block px-2 py-1 text-xs font-semibold rounded-full ${
-                          feedback.sentiment === "Positive"
+                          feedback.sentiment === "positive"
                             ? "bg-green-100 text-green-800"
-                            : feedback.sentiment === "Neutral"
+                            : feedback.sentiment === "neutral"
                             ? "bg-yellow-100 text-yellow-800"
                             : "bg-red-100 text-red-800"
                         }`}
@@ -186,7 +188,8 @@ const Feedback = () => {
                         {feedback.sentiment}
                       </span>
                     </TableCell>
-                    <TableCell className="max-w-md truncate">
+                    <TableCell>{feedback.score}</TableCell>
+                    <TableCell className="max-w-sm truncate">
                       {feedback.feedback}
                     </TableCell>
                     <TableCell>{feedback.date}</TableCell>
@@ -199,7 +202,71 @@ const Feedback = () => {
                         </DropdownMenuTrigger>
                         <DropdownMenuContent align="end">
                           <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                          <DropdownMenuItem>View Details</DropdownMenuItem>
+                          <Dialog>
+                            <DialogTrigger asChild>
+                              <DropdownMenuItem
+                                onSelect={(e) => e.preventDefault()}
+                              >
+                                View Details
+                              </DropdownMenuItem>
+                            </DialogTrigger>
+                            <DialogContent className="sm:max-w-[500px]">
+                              <DialogHeader>
+                                <DialogTitle>Feedback Details</DialogTitle>
+                                <DialogDescription>
+                                  Full details of the selected feedback
+                                </DialogDescription>
+                              </DialogHeader>
+                              <div className="grid gap-4 py-4">
+                                {!!feedback.user && (
+                                  <div className="flex items-center gap-4">
+                                    <Label className="text-right">User</Label>
+                                    <div className="col-span-3">
+                                      {feedback.user}
+                                    </div>
+                                  </div>
+                                )}
+                                {!!feedback.email && (
+                                  <div className="flex items-center gap-4">
+                                    <Label className="text-right">Email</Label>
+                                    <div className="col-span-3">
+                                      {feedback.email}
+                                    </div>
+                                  </div>
+                                )}
+                                <div className="flex items-center gap-4">
+                                  <Label className="text-right">
+                                    Sentiment
+                                  </Label>
+                                  <div className="col-span-3">
+                                    <span
+                                      className={`inline-block px-2 py-1 text-xs font-semibold rounded-full ${
+                                        feedback.sentiment === "positive"
+                                          ? "bg-green-100 text-green-800"
+                                          : feedback.sentiment === "neutral"
+                                          ? "bg-yellow-100 text-yellow-800"
+                                          : "bg-red-100 text-red-800"
+                                      }`}
+                                    >
+                                      {feedback.sentiment}
+                                    </span>
+                                  </div>
+                                </div>
+                                <div className="flex items-center gap-4">
+                                  <Label className="text-right">Date</Label>
+                                  <div className="col-span-3">
+                                    {feedback.date}
+                                  </div>
+                                </div>
+                                <div className="">
+                                  <Label className="text-right">Feedback</Label>
+                                  <div className="col-span-3">
+                                    {feedback.feedback}
+                                  </div>
+                                </div>
+                              </div>
+                            </DialogContent>
+                          </Dialog>
                           <DropdownMenuSeparator />
                           <DropdownMenuItem className="text-red-600">
                             Delete
@@ -211,6 +278,20 @@ const Feedback = () => {
                 ))}
               </TableBody>
             </Table>
+            <div className="flex gap-x-4 mt-4">
+              <Link
+                to={`?page=${page - 1}&sentiment=${selectedSentiment}`}
+                className={!hasPreviousPage ? "pointer-events-none" : ""}
+              >
+                <Button disabled={!hasPreviousPage}>Previous</Button>
+              </Link>
+              <Link
+                to={`?page=${page + 1}&sentiment=${selectedSentiment}`}
+                className={!hasNextPage ? "pointer-events-none" : ""}
+              >
+                <Button disabled={!hasNextPage}>Next</Button>
+              </Link>
+            </div>
           </CardContent>
         </Card>
       </div>
