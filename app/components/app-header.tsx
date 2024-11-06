912cadf60692da657dde38a834bec02dc79939b6
akinloluwami <akinkunmioye42@gmail.com>
2024-11-06 08:47:29 +0100

    feat: add sidebar toggle icon for mobile

--- app/components/app-header.tsx
+++ app/components/app-header.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { SidebarTrigger } from "./ui/sidebar";
 
 const AppHeader = ({ title }: { title: string }) => {
   return (
     <div className="bg-accent-base text-white shadow-sm w-full px-4 sm:px-6 lg:px-8 py-4 sticky top-0 z-10">
-      <h2 className="text-xl font-semibold">{title}</h2>
+      <div className="flex items-center gap-x-2">
+        <SidebarTrigger className="lg:hidden flex" />
+        <h2 className="lg:text-xl text-lg font-semibold">{title}</h2>
+      </div>
     </div>
   );
 };
