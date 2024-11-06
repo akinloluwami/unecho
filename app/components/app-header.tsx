09ec14efaec72d3b160bfde219fe961ccdd05432
akinloluwami <akinkunmioye42@gmail.com>
2024-11-06 08:01:14 +0100

    style: change header background color

--- app/components/app-header.tsx
+++ app/components/app-header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AppHeader = ({ title }: { title: string }) => {
   return (
-    <div className="bg-white shadow-sm w-full px-4 sm:px-6 lg:px-8 py-4 sticky top-0 z-10">
+    <div className="bg-accent-base text-white shadow-sm w-full px-4 sm:px-6 lg:px-8 py-4 sticky top-0 z-10">
       <h2 className="text-xl font-semibold">{title}</h2>
     </div>
   );
